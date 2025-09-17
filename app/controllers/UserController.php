dda4c2b50216a2a741518f553f2574a6c2aec49b
Jhon Lawrence <salazarjhonlawrence@example.com>
2025-09-17 15:43:56 +0800

    3rd commit

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller {
     public function index()
     {
         $data = $this->UsersModel->all();
-        var_dump(['users' => $data]);exit;
         $this->call->view('users/index', ['users' => $data]);
     }
 
